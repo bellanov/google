65f2779c681519270d32c90473aa31754a95b129
Bellanov Apilli <bellanovapilli@gmail.com>
2026-08-24 14:18:09 -0400

    Initial Organization View

---
+++ tui/domain/views/organization.py
@@ -0,0 +1,27 @@
+"""Organization View."""
+
+import os
+
+from tui.domain.models.errors import EnvironmentVariableError
+from tui.domain.models.organization import GCP_ORGANIZATION
+from tui.services.organization import get_organization
+
+GCP_ORGANIZATION = os.environ.get("GCP_ORGANIZATION")
+
+if not GCP_ORGANIZATION:
+    raise EnvironmentVariableError("GCP_ORGANIZATION environment variable is not set.")
+
+
+ORGANIZATION = get_organization(GCP_ORGANIZATION)
+print(f"Organization: {ORGANIZATION}")
+
+ORGANIZATION_MARKDOWN = f"""\
+## {ORGANIZATION.display_name}
+
+## Organization
+
+|             |                         |
+| --------------- | ---------------------------- |
+| name      | {ORGANIZATION.name}   |
+| display_name   | {ORGANIZATION.display_name}   |
+"""
